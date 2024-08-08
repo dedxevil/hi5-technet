1c6b0ea5d47a167d57e3ed4dbd926f0bf97bac12
Subash Chandran <schandran@axidio.com>
2024-08-08 22:14:00 +0530

    Changed web url

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,6 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://master--kreatif-software.netlify.app",
+  site: "https://dedxevil.github.io/hi5-technet",
   integrations: [tailwind({ configFile: "./tailwind.config.mjs" })],
 });
